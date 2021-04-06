874e18bc4539beb18dfd0b8626b8eaee01954184
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-04-06 11:38:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const pText = document.getElementbyId("text");
 function loadText() {
-pText.innerHTML = "This is really cool!";
+pText.textContent = "This is really cool!";
 }
 
 document.addEventListener("DOMContentLoaded", loadText);
